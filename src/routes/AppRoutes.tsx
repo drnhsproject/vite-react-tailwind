1f77b3b890823897b71c47297e0e7ab681c8a815
Zulfikar Ali Muzakir <zulfikar26error@gmail.com>
2025-06-16 20:47:43 +0700

    feat: initial dashboard structure

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,6 @@
 import App from '@/App';
 import { LoginPage } from '@/modules/auth/pages/LoginPage';
+import Page from '@/pages/dashboard/page';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export function AppRoutes() {
@@ -8,6 +9,7 @@ export function AppRoutes() {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Page />} />
       </Routes>
     </BrowserRouter>
   );
